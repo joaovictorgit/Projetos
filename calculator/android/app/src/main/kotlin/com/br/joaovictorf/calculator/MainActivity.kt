407771cc80ac67e289b12af61c206fe840cec7a1
joaovictorgit <victoralmeida.0216@gmail.com>
2021-11-30 08:07:03 -0300

    Calculator

---
+++ calculator/android/app/src/main/kotlin/com/br/joaovictorf/calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.br.joaovictorf.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ calculator/lib/main.dart
@@ -0,0 +1,9 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'screens/calculator.dart';
+
+
+void main() {
+  runApp(Calculator());
+}
+
